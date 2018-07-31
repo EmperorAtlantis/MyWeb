99332a7a8234311bcf0306f91a06d8b38076bef8
EmperorAtlantis <1968297406@qq.com>
2018-07-31 22:08:52 +0800

    180731 22.08

---
+++ NH.Data/NHibernateHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace NH.Data
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        public static ISessionFactory SessionFactory => _sessionFactory ?? (new Configuration()).Configure().BuildSessionFactory();
+    }
+}
